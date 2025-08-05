08b829660f037578d9a50a618ae68dd3d0795ed4
Songning Lai <202000120172@mail.sdu.edu.cn>
2025-08-05 17:10:50 +0800

    Delete Analytic-continual-learning directory

--- Analytic-continual-learning/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .validate import validate
-from .set_weight_decay import set_weight_decay
-from .set_determinism import set_determinism
-from .metrics import ClassificationMeter
-
-__all__ = ["validate", "set_weight_decay", "set_determinism", "ClassificationMeter"]
